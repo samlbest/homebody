0de3a2d4bd448ee47d4d8e7bd7917e6166f01120
Sam Best <sambest86@gmail.com>
2015-07-08 18:38:36 -0500

    Add isMacConnected function

--- examples/example.js
+++ examples/example.js
@@ -4,3 +4,7 @@ var homebody = new h({});
 homebody.getConnectedMacAddrs(function(result) {
   console.log(result);
 }); 
+
+homebody.isMacConnected('B0:05:94:04:CA:75', function(result) {
+  console.log('mac connected: ' + result);
+});
